edc98e253fc4b4d537fc04b487b8f68ffb28529f
Ksenia Mayakovskaya <kseniadmay@gmail.com>
2026-02-19 01:06:21 +0300

    Прошла урок по подключению статических файлов

--- sitewomen/sitewomen/settings.py
+++ sitewomen/sitewomen/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5hc814&08ut^-@zlb2iij5bsj1%&bvv6an*4#x#4l$ly40mrf#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
